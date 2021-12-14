908ed95d10175c1a0c4127d377f4d3fc7f6a4523
Aswene Sivaraj <ashsiva4@hotmail.co.uk>
2021-12-14 12:47:06 +0000

    removed node modules

--- back-end/index.js
+++ back-end/index.js
@@ -11,14 +11,9 @@ app.use(bodyParser.json());
 const routes = require('./Routes/Films');
 app.use(routes);
 
-// // Hello Path for testing
-// app.use("/hello", (req,res,next) => {
-//     res.send("hi");
-// })
-
 // Communicating with port and checking connection
 const server = app.listen(3005, () => {
-    console.log(`Server has successfully started on port number: ${server.address().port}`);
+    console.log(`Server has successfully started on port: ${server.address().port}`);
 });
 
 // Exporting app
